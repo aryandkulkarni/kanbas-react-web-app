a13cda096c25d184fd1176f55d85f357aeaf363d
AryanKulkarni <kulkarni.arya@northeastern.edu>
2024-10-14 04:10:10 -0400

    Lab

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -2,9 +2,24 @@ import "./index.css";
 import ForegroundColors from "./ForegroundColors";
 import BackgroundColors from "./BackgroundColors";
 import Borders from "./Borders";
+import Padding from "./Padding";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Positions from "./Positions";
+import Float from "./Float";
+import GridLayout from "./GridLayout";
+import Flex from "./Flex";
+import ReactIcons from "./ReactIcons";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation"
+import React from "react";
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <div className="container">
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p //style={{ backgroundColor: "blue",
@@ -59,8 +74,21 @@ export default function Lab2() {
       <ForegroundColors />
       <BackgroundColors />
       <Borders />
+      <Padding />
+      <Corners />
+      <Dimensions />
+      <Positions />
+      <Float />
+      <GridLayout />
+      <Flex />
+      <ReactIcons />
+      <BootstrapGrids />
+      <ScreenSizeLabel />
+      <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
+      <BootstrapNavigation />
     </div>
   );
 }
-
 
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,6 +4,7 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import React from "react";
 export default function Labs() {
   return (
     <div>
