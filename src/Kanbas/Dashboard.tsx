4c19bf6948c973793e9005684ee489f753ff022e
AryanKulkarni <kulkarni.arya@northeastern.edu>
2024-10-15 18:11:21 -0400

    Kanbas

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,125 +1,96 @@
 import { Link } from "react-router-dom";
-import "./Dashboard.css"
+import "./Dashboard.css";
+
 export default function Dashboard() {
+  const courses = [
+    {
+      id: 1234,
+      title: "CS1234 React JS",
+      code: "CS1234",
+      term: "Fall",
+      year: "2024",
+      section: "001",
+      image: "/images/reactjs.jpg",
+    },
+    {
+      id: 1235,
+      title: "CS1235 GoLang",
+      code: "CS1235",
+      term: "Fall",
+      year: "2024",
+      section: "002",
+      image: "/images/golang.jpg",
+    },
+    {
+      id: 1236,
+      title: "CS1236 C#",
+      code: "CS1236",
+      term: "Fall",
+      year: "2024",
+      section: "003",
+      image: "/images/csharp.jpg",
+    },
+    {
+      id: 1237,
+      title: "CS1237 Rust",
+      code: "CS1237",
+      term: "Fall",
+      year: "2024",
+      section: "004",
+      image: "/images/rust.jpg",
+    },
+    {
+      id: 1238,
+      title: "CS1238 Haskell",
+      code: "CS1238",
+      term: "Fall",
+      year: "2024",
+      section: "005",
+      image: "/images/haskell.jpg",
+    },
+    {
+      id: 1239,
+      title: "CS1239 OCaml",
+      code: "CS1239",
+      term: "Fall",
+      year: "2024",
+      section: "006",
+      image: "/images/ocaml.jpg",
+    },
+    {
+      id: 1230,
+      title: "CS1230 Racket",
+      code: "CS1230",
+      term: "Fall",
+      year: "2024",
+      section: "007",
+      image: "/images/racket.jpg",
+    },
+  ];
+
   return (
     <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-            to="/Kanbas/Courses/1234/Home">
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5>
-                CS1234 React JS
-              </h5>
-              <p className="wd-dashboard-course-title">
-                CS1234
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-            to="/Kanbas/Courses/1235/Home">
-            <img src="/images/golang.jpg" width={200} />
-            <div>
-              <h5>
-                CS1235 GoLang
-              </h5>
-              <p className="wd-dashboard-course-title">
-                CS1235
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-            to="/Kanbas/Courses/1236/Home">
-            <img src="/images/csharp.jpg" width={200} />
-            <div>
-              <h5>
-                CS1236 C#
-              </h5>
-              <p className="wd-dashboard-course-title">
-                CS1236
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-            to="/Kanbas/Courses/1237/Home">
-            <img src="/images/rust.jpg" width={200} />
-            <div>
-              <h5>
-                CS1237 Rust
-              </h5>
-              <p className="wd-dashboard-course-title">
-                CS1237
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-            to="/Kanbas/Courses/1238/Home">
-            <img src="/images/haskell.jpg" width={200} />
-            <div>
-              <h5>
-                CS1238 Haskell
-              </h5>
-              <p className="wd-dashboard-course-title">
-                CS1238
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-            to="/Kanbas/Courses/1239/Home">
-            <img src="/images/ocaml.jpg" width={200} />
-            <div>
-              <h5>
-                CS1239 OCaml
-              </h5>
-              <p className="wd-dashboard-course-title">
-                CS1239
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-            to="/Kanbas/Courses/1230/Home">
-            <img src="/images/racket.jpg" width={200} />
-            <div>
-              <h5>
-                CS1230 Racket
-              </h5>
-              <p className="wd-dashboard-course-title">
-                CS1230
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
+        {courses.map((course) => (
+          <div className="wd-dashboard-course" key={course.id}>
+            <Link
+              className="wd-dashboard-course-link"
+              to={`/Kanbas/Courses/${course.id}/Home`}
+            >
+              <img src={course.image} alt={course.title} />
+              <div>
+                <h5>{course.title}</h5>
+                <p className="wd-dashboard-course-title">
+                  {course.term} {course.year} - Section {course.section}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
-
-
-
-
-
-
-
-
