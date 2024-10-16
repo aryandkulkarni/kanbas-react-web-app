0de68a95b266bf96f116a91f0dcc7ed2d4ef1604
AryanKulkarni <kulkarni.arya@northeastern.edu>
2024-10-15 20:50:43 -0400

    Kanbas

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,111 +1,36 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function AssignmentEditor() {
     return (
-        <div id="wd-assignments-editor">
+        <div id="wd-assignments-editor" className="container mt-4">
             <h1>Course 1234</h1>
-
-            <label htmlFor="wd-name">Assignment Name </label><br /><br />
-            <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
-            <textarea id="wd-textarea" cols={30} rows={10}>The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositories The Kanbas application should include a link to navigate back to the landing page.</textarea>
-            <br /><br />
-            <table>
-                <tbody>
-                    <tr>
-                        <td align="right" valign="top">
-                            <label htmlFor="wd-points">Points</label>
-                        </td>
-                        <td>
-                            <input id="wd-points" value={100} />
-                        </td>
-                    </tr>
-                    <br /><br />
-                    <tr>
-                        <td align="right" valign="top">
-                            <label htmlFor="wd-group">Assignment Group</label>
-                        </td>
-                        <td>
-                            <select id="wd-group">
-                                <option value="assignments">ASSIGNMENTS</option>
-                            </select>
-                        </td>
-                    </tr>
-                    <br /><br />
-                    <tr>
-                        <td align="right" valign="top">
-                            <label htmlFor="wd-display-grade">Display Grade as</label>
-                        </td>
-                        <td>
-                            <select id="wd-display-grade">
-                                <option value="percentage">Percentage</option>
-                            </select>
-                        </td>
-                    </tr>
-                    <br /><br />
-                    <tr>
-                        <td align="right" valign="top">
-                            <label htmlFor="wd-submission">Submission Type</label>
-                        </td>
-                        <td>
-                            <select id="wd-submission">
-                                <option value="online">Online</option>
-                            </select>
-                            <br /><br />
-                            <label htmlFor="wd-online">Online Entry Options </label>
-                            <div>
-                                <input type="checkbox" id="text-entry" name="text-entry" />
-                                <label htmlFor="text-entry">Text Entry</label>
-                                <br />
-                                <input type="checkbox" id="website-url" name="website-url" />
-                                <label htmlFor="website-url">Website URL</label>
-                                <br />
-                                <input type="checkbox" id="media-recordings" name="media-recordings" />
-                                <label htmlFor="media-recordings">Media Recordings</label>
-                                <br />
-                                <input type="checkbox" id="student-annotation" name="student-annotation" />
-                                <label htmlFor="student-annotation">Student Annotation</label>
-                                <br />
-                                <input type="checkbox" id="file-uploads" name="file-uploads" />
-                                <label htmlFor="file-uploads">File Uploads</label>
-                            </div>
-                        </td>
-                    </tr>
-                    <br /><br />
-                    <tr>
-                        <td align="right" valign="top">
-                            <label htmlFor="wd-assign">Assign To</label>
-                        </td>
-                        <td>
-                            <input id="wd-assign" value="Everyone" />
-                        </td>
-                    </tr>
-                    <br /><br />
-
-                    <tr>
-                        <td align="right" valign="top">
-                            <label htmlFor="wd-due">Due</label>
-                        </td>
-                        <td>
-                            <input type="date" id="wd-due" value="2024-05-13" />
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td align="right" valign="top">
-                            <label htmlFor="wd-available-from">Available from</label>
-                        </td>
-                        <td>
-                            <input type="date" id="wd-available-from" value="2024-05-06" />
-                            <label htmlFor="wd-available-until"> Until </label>
-                            <input type="date" id="wd-available-until" value="2024-05-20" />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <br />
-
-            <button>Cancel</button>
-            <button>Save</button>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="wd-name" className="form-label">Assignment Name</label>
+                    <input id="wd-name" className="form-control" value="A1 - ENV + HTML" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="wd-textarea" className="form-label">Description</label>
+                    <textarea id="wd-textarea" className="form-control" rows={4}>
+                        The assignment is available online. Submit a link to the landing page of your Web application running on Netlify.
+                    </textarea>
+                </div>
+                <div className="row mb-3">
+                    <div className="col-md-6">
+                        <label htmlFor="wd-points" className="form-label">Points</label>
+                        <input id="wd-points" className="form-control" type="number" placeholder="100" />
+                    </div>
+                    <div className="col-md-6">
+                        <label htmlFor="wd-due-date" className="form-label">Due Date</label>
+                        <input id="wd-due-date" className="form-control" type="date" />
+                    </div>
+                </div>
+                <div className="d-flex justify-content-between mt-4">
+                    <button type="button" className="btn btn-danger">Cancel</button>
+                    <button type="submit" className="btn btn-success">Save</button>
+                </div>
+            </form>
         </div>
     );
 }
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,50 +1,59 @@
+import React from "react";
+import { FaSearch, FaPlus, FaCheckCircle } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function Assignments() {
   return (
-    <div id="wd-assignments">
-      <input id="wd-search-assignment"
-        placeholder="Search for Assignments" />
-      <button id="wd-add-assignment-group">+ Group</button>
-      <button id="wd-add-assignment">+ Assignment</button>
-      <h3 id="wd-assignments-title">
-        ASSIGNMENTS 40% of Total <button>+</button>
-      </h3>
-      <ul id="wd-assignment-list">
-        <li className="wd-assignment-list-item">
-          <a className="wd-assignment-link"
-            href="#/Kanbas/Courses/1234/Assignments/123">
-            A1 - ENV + HTML
-          </a>
-          <br />
-          Multiple Modules | Not available until May 6 at 12:00am
-          <br />
-          Due May 13 at 11:59pm | 100 pts
+    <div id="wd-assignments" className="container mt-4">
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <div className="input-group" style={{ width: "250px" }}>
+          <span className="input-group-text">
+            <FaSearch />
+          </span>
+          <input
+            id="wd-search-assignment"
+            className="form-control"
+            placeholder="Search for Assignments"
+          />
+        </div>
+        <div>
+          <button className="btn btn-outline-secondary me-2">
+            <FaPlus /> Group
+          </button>
+          <button className="btn btn-danger">
+            <FaPlus /> Assignment
+          </button>
+        </div>
+      </div>
+      <h5 id="wd-assignments-title" className="mb-3">
+        ASSIGNMENTS 40% of Total
+      </h5>
+      <ul id="wd-assignment-list" className="list-group">
+        <li className="list-group-item d-flex justify-content-between align-items-start border-start border-success mb-2">
+          <div>
+            <a className="wd-assignment-link text-decoration-none" href="#/Kanbas/Courses/1234/Assignments/123">
+              A1 - ENV + HTML
+            </a>
+            <br />
+            <small>Multiple Modules | Not available until May 6 at 12:00am</small>
+            <br />
+            <small>Due May 13 at 11:59pm | 100 pts</small>
+          </div>
+          <FaCheckCircle className="text-success" size={20} />
         </li>
-        <li className="wd-assignment-list-item">
-          <a
-            className="wd-assignment-link"
-            href="#/Kanbas/Courses/1234/Assignments/124"
-          >
-            A2 - CSS + BOOTSTRAP
-          </a>
-          <br />
-          Multiple Modules | Not available until May 13 at 12:00am
-          <br />
-          Due May 20 at 11:59pm | 100 pts
-        </li>
-        <li className="wd-assignment-list-item">
-          <a
-            className="wd-assignment-link"
-            href="#/Kanbas/Courses/1234/Assignments/125"
-          >
-            A3 - JAVASCRIPT + REACT
-          </a>
-          <br />
-          Multiple Modules | Not available until May 20 at 12:00am
-          <br />
-          Due May 27 at 11:59pm | 100 pts
+        <li className="list-group-item d-flex justify-content-between align-items-start border-start border-success mb-2">
+          <div>
+            <a className="wd-assignment-link text-decoration-none" href="#/Kanbas/Courses/1234/Assignments/124">
+              A2 - CSS + BOOTSTRAP
+            </a>
+            <br />
+            <small>Single Module | Available until June 1 at 12:00am</small>
+            <br />
+            <small>Due June 8 at 11:59pm | 100 pts</small>
+          </div>
+          <FaCheckCircle className="text-success" size={20} />
         </li>
       </ul>
     </div>
   );
 }
-
