2d09ce366bfccec4d9fc8504ccc13d9a951f9c74
AryanKulkarni <kulkarni.arya@northeastern.edu>
2024-12-12 15:04:42 -0500

    a5

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -3,7 +3,10 @@ export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 const axiosWithCredentials = axios.create({ withCredentials: true });
 
-
+export const findMyCourses = async () => {
+  const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
+  return data;
+};
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
   return response.data;
@@ -24,4 +27,7 @@ export const signout = async () => {
     const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
     return response.data;
 };
-  
+export const createCourse = async (course: any) => {
+  const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
+  return data;
+};
--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -5,4 +5,28 @@ export const fetchAllCourses = async () => {
   const { data } = await axios.get(COURSES_API);
   return data;
 };
+export const deleteCourse = async (id: string) => {
+    const { data } = await axios.delete(`${COURSES_API}/${id}`);
+    return data;
+  };
+  export const updateCourse = async (course: any) => {
+    const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
+    return data;
+  };
+  export const findAssignmentsForCourse = async (courseId: string) => {
+    const response = await axios.get(`${COURSES_API}/${courseId}/assignments`);
+    return response.data;
+  };
+  export const createAssignmentForCourse = async (
+    courseId: string,
+    assignment: any
+  ) => {
+    const response = await axios.post(
+      `${COURSES_API}/${courseId}/assignments`,
+      assignment
+    );
+    return response.data;
+  };
+  
+  
 
